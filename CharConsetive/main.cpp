c52bbe7ab80af2e46d7b38e710cc8ad247d26d81
sunjinglin <jinglsun1@gmail.com>
2020-07-26 21:11:53 +0800

    Initial Commit

---
+++ CharConsetive/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CharConsetive
+//
+//  Created by jinglin sun on 2020/7/26.
+//  Copyright © 2020 jinglin sun. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
